f594f184de27878ac73ba36786576f88a8930c5f
RishabhKodes <rishabhbhandari6@gmail.com>
2020-05-13 16:12:07 +0530

    updated api code

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ const channelForm = document.getElementById('channel-form');
 const channelInput = document.getElementById('channel-input');
 const videoContainer = document.getElementById('video-container');
 
+
+
 //auth2 library
 function handleClientLoad(){
   gapi.load('client:auth2', initClient);
@@ -17,5 +19,45 @@ function handleClientLoad(){
 
 //initialize client library API
 function initClient(){
-  gapi.client.init
+  gapi.client.init({
+    discoveryDocs: DISCOVERY_DOCS,
+    clientId:CLIENT_ID,
+    scope:SCOPES
+  }).then(()=>{
+    //listen for state changes
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+    // handle initial signin state
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    authorizeButton.onclick = handleAuthClick;
+    signoutButton.onclick = handleSignoutClick;
+  })
+}
+
+
+function updateSigninStatus(isSignedIn){
+
+  if(isSignedIn){
+      authorizeButton.style.display = 'none';
+      signoutButton.style.display = 'block';
+      content.style.display = 'block';
+      authorizeButton.style.display = 'block';
+      getChannel(defaultChannel);
+    }else{
+
+    authorizeButton.style.display = 'block';
+    signoutButton.style.display = 'none';
+    content.style.display = 'none';
+    authorizeButton.style.display = 'none';     
+  }
+}
+
+//login
+function handleAuthClick(){
+
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+//logout
+function handleSignoutClick(){
+  gapi.auth2.getAuthInstance().signOut();
 }
